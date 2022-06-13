5a099e33633fb7b5094f2065f9c82b485cb8f523
bronzo97 <danilobronzo97@hotmail.it>
2022-06-13 16:48:45 +0200

    milestone 1 done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+const listElements = [
+{
+    text: 'Fare la spesa',
+    done: true,
+},
+{
+    text: "Fare l'esercizio di oggi",
+    done: false,
+},
+
+]
+
+
+
 const mainVue = new Vue({
     el: "#app",
 
     data: {
-
+        listItems: listElements,
 },
     methods: {
 
